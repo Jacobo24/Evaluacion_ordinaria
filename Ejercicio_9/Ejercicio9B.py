ad24ac18e0599ab8fe0871abfab7a204c3011e44
Jacobo24 <jaccalgar@gmail.com>
2023-01-18 20:45:49 +0100

    Borrar la forma a de hacer el ejercicio 9 y crear una nueva carpeta con dos formas de hacerlo

---
+++ Ejercicio_9/Ejercicio9B.py
@@ -0,0 +1,15 @@
+import datetime
+
+def number_of_mondays(birthday, current_date):
+    age = current_date.year - birthday.year - ((current_date.month, current_date.day) < (birthday.month, birthday.day))
+    if age < 22 or age > 78:
+        return 0
+    days_between_dates = (current_date - birthday).days
+    number_of_mondays = days_between_dates // 7
+    return number_of_mondays
+
+if __name__ == '__main__':
+    birthday = datetime.date(1998, 7, 15)
+    current_date = datetime.date(2022, 1, 10)
+    mondays = number_of_mondays(birthday, current_date)
+    print(mondays)
