bd5fa7bf64d9b063146a7d5b5e882fb615e42ac6
Jacobo24 <jaccalgar@gmail.com>
2023-01-19 12:15:36 +0100

    Modificación del código 8 para que la frase sea selccionada al azar

--- ejercico_8.py
+++ ejercico_8.py
@@ -1,20 +1,23 @@
 import re
 import random
 
-frases = ["El artista mira hacia arriba. \"Siempre va a tratarse de Tree Fiddy\"",
-          "¿Cuál es el precio de una cerveza?",
-          "¿Cuánto cuesta una habitación para una noche?",
-          "¿Puedo tener una cucharada de azúcar?",
-          "¿Qué tal si nos tomamos una cerveza juntos?"]
+def main():
+    frases = ["El artista mira hacia arriba. \"Siempre va a tratarse de Tree Fiddy\"",
+              "¿Cuál es el precio de una cerveza?",
+              "¿Cuánto cuesta una habitación para una noche?",
+              "¿Puedo tener una cucharada de azúcar?",
+              "¿Qué tal si nos tomamos una cerveza juntos?"]
 
-# elegir una frase al azar
-frase_elegida = random.choice(frases)
-print("Frase elegida: ", frase_elegida)
+    # elegir una frase al azar
+    frase_elegida = random.choice(frases)
+    print("Frase elegida: ", frase_elegida)
 
-# buscar "tree fiddy", "3.50" o "three fifty" en la frase elegida
-result = re.search("tree fiddy|3\.50|three fifty", frase_elegida, re.IGNORECASE)
+    # buscar "tree fiddy", "3.50" o "three fifty" en la frase elegida
+    result = re.search("tree fiddy|3\.50|three fifty", frase_elegida, re.IGNORECASE)
 
-if result:
-    print("Encontrado el Monstruo del Lago Ness!")
-else:
-    print("No se encontró el Monstruo del Lago Ness.")
+    if result:
+        print("Encontrado el Monstruo del Lago Ness!")
+    else:
+        print("No se encontró el Monstruo del Lago Ness.")
+
+    main()
