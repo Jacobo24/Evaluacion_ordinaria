ef04aa2c25fcf7c59050702d2fa539edfdc0b612
Jacobo24 <jaccalgar@gmail.com>
2023-01-19 12:10:36 +0100

    nada

--- ejercico_8.py
+++ ejercico_8.py
@@ -1,9 +1,18 @@
 import re
+import random
 
-text = "El artista mira hacia arriba. \"Siempre va a tratarse de Tree Fiddy\""
+frases = ["El artista mira hacia arriba. \"Siempre va a tratarse de Tree Fiddy\"",
+          "¿Cuál es el precio de una cerveza?",
+          "¿Cuánto cuesta una habitación para una noche?",
+          "¿Puedo tener una cucharada de azúcar?",
+          "¿Qué tal si nos tomamos una cerveza juntos?"]
 
-# buscar "tree fiddy", "3.50" o "three fifty" en el texto
-result = re.search("tree fiddy|3\.50|three fifty", text, re.IGNORECASE)
+# elegir una frase al azar
+frase_elegida = random.choice(frases)
+print("Frase elegida: ", frase_elegida)
+
+# buscar "tree fiddy", "3.50" o "three fifty" en la frase elegida
+result = re.search("tree fiddy|3\.50|three fifty", frase_elegida, re.IGNORECASE)
 
 if result:
     print("Encontrado el Monstruo del Lago Ness!")
